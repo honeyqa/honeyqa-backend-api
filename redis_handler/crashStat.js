a1183d6c026ff47665f49ea7b7b7b5dc6140509b
Minwoo Jung <jmwsoft@gmail.com>
2015-10-01 10:49:53 +0900

    add redis Crash Handler

---
+++ redis_handler/crashStat.js
@@ -0,0 +1,190 @@
+
+var redis = require('redis');
+
+
+function HoneyStat(crashID, options, redisOptions) {
+  options || (options = {});
+
+  this.crashID = crashID;
+  this.pageSize = options.pageSize || 50;
+  this.reverse = options.reverse || false;
+
+  this.connect(redisOptions);
+}
+
+
+var proto = HoneyStat.prototype;
+
+
+proto.connect = function(options) {
+  options || (options = {});
+
+
+  if (options.connection_id !== undefined &&
+      options.connected !== undefined &&
+      options.ready !== undefined) {
+    return this.redis = options;
+  }
+
+  this.redis = redis.createClient(options.port, options.host);
+
+  if (options.db) {
+    this.redis.select(options.db);
+  }
+};
+
+
+proto.add = function(crashSet, crashCount, cb) {
+  this.addIn(this.crashID, crashSet, crashCount, cb);
+};
+
+proto.addIn = function(crashID, crashSet, crashCount, cb) {
+  this.redis.zadd([crashID, crashCount, crashSet], function(err) {
+    if (err && cb) return cb(err);
+    if (err) throw err;
+    if (cb) cb();
+  });
+};
+
+
+proto.incr = function(crashSet, crashCount, cb) {
+  this.incrIn(this.crashID, crashSet, crashCount, cb);
+};
+
+proto.incrIn = function(crashID, crashSet, crashCount, cb) {
+  this.redis.zincrby([crashID, crashCount, crashSet], function(err) {
+    if (err && cb) return cb(err);
+    if (err) throw err;
+    if (cb) cb();
+  });
+};
+
+
+proto.rank = function(crashSet, cb) {
+  this.rankIn(this.crashID, crashSet, cb);
+};
+
+proto.rankIn = function(crashID, crashSet, cb) {
+  var req = [crashID, crashSet];
+
+  if (this.reverse) {
+    this.redis.zrank(req, res);
+  } else {
+    this.redis.zrevrank(req, res);
+  }
+
+  function res(err, rank) {
+    if (err) return cb(err);
+    if (rank === null)
+      cb(null, -1);
+    else
+      cb(null, +rank);
+  }
+};
+
+
+proto.crashCount = function(crashSet, cb) {
+  this.crashCountIn(this.crashID, crashSet, cb);
+};
+
+proto.crashCountIn = function(crashID, crashSet, cb) {
+  this.redis.zcrashCount([crashID, crashSet], function(err, crashCount) {
+    if (err) return cb(err);
+    if (crashCount === null)
+      cb(null, -1);
+    else
+      cb(null, +crashCount);
+  });
+};
+
+
+proto.rm = function(crashSet, cb) {
+  this.rmIn(this.crashID, crashSet, cb);
+};
+
+proto.rmIn = function(crashID, crashSet, cb) {
+  this.redis.zrem([crashID, crashSet], function(err, num) {
+    if (err && cb) return cb(err);
+    if (err) throw err;
+    if (cb) cb(null, !!num);
+  });
+};
+
+
+proto.list = function(page, cb) {
+  this.listIn(this.crashID, page, cb);
+};
+
+proto.listIn = function(crashID, page, cb) {
+  if (typeof(cb) === 'undefined' && page instanceof Function) {
+    cb = page;
+  }
+  if (typeof(page) === 'undefined' || page instanceof Function) {
+    page = 0;
+  }
+
+  var req = [
+    crashID
+  , page * this.pageSize
+  , page * this.pageSize + this.pageSize - 1
+  , 'WITHcrashCountS'
+  ];
+
+  if (this.reverse) {
+    this.redis.zrange(req, res);
+  } else {
+    this.redis.zrevrange(req, res);
+  }
+
+  function res(err, range) {
+    if (err) return cb(err);
+
+    var list = [], l = range.length;
+
+    for (var i = 0; i < l; i += 2) {
+      list.push({
+        'crashSet': range[i]
+      , 'crashCount': range[i+1]
+      });
+    }
+
+    cb(null, list);
+  }
+};
+
+
+proto.at = function(rank, cb) {
+  this.atIn(this.crashID, rank, cb);
+};
+
+proto.atIn = function(crashID, rank, cb) {
+  var req = [crashID, rank, rank, 'WITHcrashCountS'];
+
+  if (this.reverse) {
+    this.redis.zrange(req, res);
+  } else {
+    this.redis.zrevrange(req, res);
+  }
+
+  function res(err, range) {
+    if (err) return cb(err);
+    if (!range.length)
+      return cb(null, null);
+
+    cb(null, {
+      crashSet: range[0]
+    , crashCount: +range[1]
+    });
+  }
+};
+
+
+proto.total = function(cb) {
+  this.totalIn(this.crashID, cb);
+};
+
+proto.totalIn = function(crashID, cb) {
+  this.redis.zcard(crashID, cb);
+};
+
+module.exports = HoneyStat;
