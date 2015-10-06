a1bc45924de64542fe6bd771e08574165ff7d0da
devholic <devholic@plusquare.com>
2015-10-06 21:17:46 +0900

    add route for local test

--- api/api.js
+++ api/api.js
@@ -1,34 +1,31 @@
 var express = require('express');
-var app = express(),amqp = require('amqp');
+var app = express(),
+  amqp = require('amqp');
 var crash = require('../router/crash.js');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-
+app.get('/', function(req, res) {
   res.send('HoneyQA API Server');
+});
 
+// TODO : DELETE THIS ON RELEASE
+app.post('/api/client/test', function(req, res) {
+  console.log(req.body)
+  res.send(req.body)
 });
 
 app.post('/api/client/exception', function(req, res) {
-
-  crash.insertLog(req,res);
-  
+  crash.insertLog(req, res);
 });
 
-
-app.post('/api/client/exception/native', function(req, res){
-  
-  crash.insertLog(req,res);
-  
+app.post('/api/client/exception/native', function(req, res) {
+  crash.insertLog(req, res);
 });
-      
 
-var server = app.listen(7707, function () {
+var server = app.listen(7707, function() {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('HoneyQA REST API Server Started:%s', port);
-
 });
