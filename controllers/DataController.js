e8275d1d55dc9211769de9f7853d35e59fbfb1e8
Minwoo Jung <jmwsoft@gmail.com>
2015-10-24 13:01:59 +0900

    add frontend api
    add frontend api

--- controllers/DataController.js
+++ controllers/DataController.js
@@ -42,6 +42,122 @@ var router = express.Router();
  		return res.json(objects);
  	});
  });
+ 
+ 
+router.get(routeIdentifier+'/appruncount/weekly/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select * from appruncount where project_id = ? and date >= now() - interval 1 week order by date';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+	 	
+	 	var result = new Object();
+		var weeklyArr = [];
+
+	 	for(var i=0; i<rows.length; i++){
+	 		var element = new Object();
+	 		element.error_count = rows[i].error_count;
+	 		element.session_count = rows[i].session_count;
+	 		element.date = rows[i].date;
+	 		weeklyArr.push(element);
+	 	}
+
+	 	result.weekly = weeklyArr;
+		res.send(result);
+	});
+});
+
+router.get(routeIdentifier+'/most/sessionbyappver/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select appversion, count(*) as count from sessions where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+
+		var result = new Object();
+		result = rows[0];
+
+		res.send(result);
+	});
+});
+
+router.get(routeIdentifier+'/most/errorbyappver/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select appversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+
+		var result = new Object();
+		result = rows[0];
+		
+		res.send(result);
+	});
+});
+
+router.get(routeIdentifier+'/most/errorbydevice/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select device, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by device order by count(*) desc limit 1';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+
+		var result = new Object();
+		result = rows[0];
+		
+		res.send(result);
+	});
+});
+
+router.get(routeIdentifier+'/most/errorbysdkversion/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select sdkversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by sdkversion order by count(*) desc limit 1';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+
+		var result = new Object();
+		result = rows[0];
+		
+		res.send(result);
+	});
+});
+
+router.get(routeIdentifier+'/most/errorbycountry/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select country, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by country order by count(*) desc limit 1';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+
+		var result = new Object();
+		result = rows[0];
+		
+		res.send(result);
+	});
+});
+
+router.get(routeIdentifier+'/most/errorbyclassname/:id', function(req, res){
+	var key = req.params.id;
+	var queryString = 'select lastactivity, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by lastactivity order by count(*) desc limit 1';
+	connection.query(queryString, [key], function(err, rows, fields){
+		if(err) throw err;
+
+		res.header('Access-Control-Allow-Origin', '*');
+
+		var result = new Object();
+		result = rows[0];
+		
+		res.send(result);
+	});
+});
+
 
  router.get(routeIdentifier+'/error_instances/weekly/:id', function (req, res, next) {
    var key = req.params.id;
