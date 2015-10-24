2fedc97960839b8ede95f294353eea71d53a62ad
Minwoo Jung <jmwsoft@gmail.com>
2015-10-24 13:02:47 +0900

    renew frontend api

--- front/frontend_api.js
+++ front/frontend_api.js
@@ -1,30 +1,50 @@
+
+var modelLocation = '../models/Data'
+var async = require('async');
+var gk = require('../common');
+var mq_pubhandler = require('../handler/mq_pubhandler');
+var util = require('util');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	multipleStatements : 'true'
-});
-var server = app.listen(8080, function(){
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Honeyqa API Server Started: %s', port);
-});
+var authController = require('./AuthController');
 
-connection.connect(function(err){
-	if(!err){
-		console.log('Database is connected ... \n\n');
-	}else{
-		console.log('Error connecting database ... \n\n');
-	}
-});
+/**  Model and route setup **/
 
+var model = require(modelLocation).model;
+var userModel = require('../models/User').model;
 
-app.get('/', function(req, res){
-	res.send('Honeyqa API');
-});
+const route = require(modelLocation).route;
+const routeIdentifier = util.format('/%s', route);
 
-app.get('/appruncount/weekly/:id', function(req, res){
+/** Express setup **/
+
+var router = express.Router();
+
+/** Express routing **/
+
+ router.use('*', function (req, res, next) {
+ 	if (!req.user) {
+        return res.status(403).send('HoenyQA, 403 - Forbidden');
+    }
+
+ 	if (userModel.findOne({'_id': req.user._id}, function (err, res) {
+ 		if (err) {
+            return res.send(err);
+        }
+
+ 		next();
+ 	}));
+ });
+
+ router.get(routeIdentifier+'/list', function(req, res, next) {
+ 	model.find({'owner':req.user._id}, function (err, objects) {
+ 		if (err) return res.send(err);
+ 		return res.json(objects);
+ 	});
+ });
+ 
+ 
+router.get(routeIdentifier+'/appruncount/weekly/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select * from appruncount where project_id = ? and date >= now() - interval 1 week order by date';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -48,7 +68,7 @@ app.get('/appruncount/weekly/:id', function(req, res){
 	});
 });
 
-app.get('/most/sessionbyappver/:id', function(req, res){
+router.get(routeIdentifier+'/most/sessionbyappver/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select appversion, count(*) as count from sessions where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -63,7 +83,7 @@ app.get('/most/sessionbyappver/:id', function(req, res){
 	});
 });
 
-app.get('/most/errorbyappver/:id', function(req, res){
+router.get(routeIdentifier+'/most/errorbyappver/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select appversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -78,7 +98,7 @@ app.get('/most/errorbyappver/:id', function(req, res){
 	});
 });
 
-app.get('/most/errorbydevice/:id', function(req, res){
+router.get(routeIdentifier+'/most/errorbydevice/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select device, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by device order by count(*) desc limit 1';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -93,7 +113,7 @@ app.get('/most/errorbydevice/:id', function(req, res){
 	});
 });
 
-app.get('/most/errorbysdkversion/:id', function(req, res){
+router.get(routeIdentifier+'/most/errorbysdkversion/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select sdkversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by sdkversion order by count(*) desc limit 1';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -108,7 +128,7 @@ app.get('/most/errorbysdkversion/:id', function(req, res){
 	});
 });
 
-app.get('/most/errorbycountry/:id', function(req, res){
+router.get(routeIdentifier+'/most/errorbycountry/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select country, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by country order by count(*) desc limit 1';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -123,7 +143,7 @@ app.get('/most/errorbycountry/:id', function(req, res){
 	});
 });
 
-app.get('/most/errorbyclassname/:id', function(req, res){
+router.get(routeIdentifier+'/most/errorbyclassname/:id', function(req, res){
 	var key = req.params.id;
 	var queryString = 'select lastactivity, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by lastactivity order by count(*) desc limit 1';
 	connection.query(queryString, [key], function(err, rows, fields){
@@ -138,3 +158,47 @@ app.get('/most/errorbyclassname/:id', function(req, res){
 	});
 });
 
+
+ router.get(routeIdentifier+'/error_instances/weekly/:id', function (req, res, next) {
+   var key = req.params.id;
+   var queryString = 'SELECT * from error_instances where error_id = ? AND date >= now() - interval 1 week';
+
+   connection.query(queryString, [key], function(err, rows, fields) {
+   res.header("Access-Control-Allow-Origin", "*");
+   if (err) throw err;
+   res.json(rows);
+
+   });
+ });
+
+ router.get(routeIdentifier+'/appruncount/weekly/:id',, function(req, res, next) {
+   var data = { 'tag':'appruncount_weekly','data': req.params.id};
+   async.series([
+        function (cb) {
+            var ret = mq_pubhandler.publish(queueName, data);
+            cb();
+        }
+    ], function (err) {
+        var result = { 'state': 'success' };
+        res.send(result);
+    });
+
+ });
+
+// TODO : DELETE THIS ON RELEASE
+router.post(routeIdentifier+'/api/client/test', function(req, res) {
+  console.log(req.body)
+  res.send(req.body)
+});
+
+// SESSION
+router.post(routeIdentifier+'/api/client/session', function(req, res) {
+  // TODO : Handle data from client
+  // REFERENCE : https://github.com/UrQA/Api_Backand/blob/master/controllers/url_control.js#L24
+  var result = {
+    'state': 'success'
+  };
+  res.send(result);
+});
+
+module.exports = router;
