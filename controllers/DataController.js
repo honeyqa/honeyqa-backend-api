cb7ccc6b250b758f98a94decdc8f8cbd86df8221
devholic <devholic@plusquare.com>
2015-11-01 20:45:06 +0900

    beautify code

--- controllers/DataController.js
+++ controllers/DataController.js
@@ -25,173 +25,173 @@ var router = express.Router();
 /** Express routing **/
 
 router.use('*', function(req, res, next) {
-  if (!req.user) {
-    return res.status(403).send('HoenyQA, 403 - Forbidden');
-  }
+    if (!req.user) {
+        return res.status(403).send('HoenyQA, 403 - Forbidden');
+    }
 
-  if (userModel.findOne({
-      '_id': req.user._id
-    }, function(err, res) {
-      if (err) {
-        return res.send(err);
-      }
+    if (userModel.findOne({
+            '_id': req.user._id
+        }, function(err, res) {
+            if (err) {
+                return res.send(err);
+            }
 
-      next();
-    }));
+            next();
+        }));
 });
 
 router.get(routeIdentifier + '/list', function(req, res, next) {
-  model.find({
-    'owner': req.user._id
-  }, function(err, objects) {
-    if (err) return res.send(err);
-    return res.json(objects);
-  });
+    model.find({
+        'owner': req.user._id
+    }, function(err, objects) {
+        if (err) return res.send(err);
+        return res.json(objects);
+    });
 });
 
 
 router.get(routeIdentifier + '/appruncount/weekly/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select * from appruncount where project_id = ? and date >= now() - interval 1 week order by date';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select * from appruncount where project_id = ? and date >= now() - interval 1 week order by date';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    var weeklyArr = [];
+        var result = new Object();
+        var weeklyArr = [];
 
-    for (var i = 0; i < rows.length; i++) {
-      var element = new Object();
-      element.error_count = rows[i].error_count;
-      element.session_count = rows[i].session_count;
-      element.date = rows[i].date;
-      weeklyArr.push(element);
-    }
+        for (var i = 0; i < rows.length; i++) {
+            var element = new Object();
+            element.error_count = rows[i].error_count;
+            element.session_count = rows[i].session_count;
+            element.date = rows[i].date;
+            weeklyArr.push(element);
+        }
 
-    result.weekly = weeklyArr;
-    res.send(result);
-  });
+        result.weekly = weeklyArr;
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/most/sessionbyappver/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select appversion, count(*) as count from sessions where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select appversion, count(*) as count from sessions where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    result = rows[0];
+        var result = new Object();
+        result = rows[0];
 
-    res.send(result);
-  });
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/most/errorbyappver/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select appversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select appversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by appversion order by count(*) desc limit 1';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    result = rows[0];
+        var result = new Object();
+        result = rows[0];
 
-    res.send(result);
-  });
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/most/errorbydevice/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select device, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by device order by count(*) desc limit 1';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select device, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by device order by count(*) desc limit 1';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    result = rows[0];
+        var result = new Object();
+        result = rows[0];
 
-    res.send(result);
-  });
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/most/errorbysdkversion/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select sdkversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by sdkversion order by count(*) desc limit 1';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select sdkversion, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by sdkversion order by count(*) desc limit 1';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    result = rows[0];
+        var result = new Object();
+        result = rows[0];
 
-    res.send(result);
-  });
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/most/errorbycountry/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select country, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by country order by count(*) desc limit 1';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select country, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by country order by count(*) desc limit 1';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    result = rows[0];
+        var result = new Object();
+        result = rows[0];
 
-    res.send(result);
-  });
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/most/errorbyclassname/:id', function(req, res) {
-  var key = req.params.id;
-  var queryString = 'select lastactivity, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by lastactivity order by count(*) desc limit 1';
-  connection.query(queryString, [key], function(err, rows, fields) {
-    if (err) throw err;
+    var key = req.params.id;
+    var queryString = 'select lastactivity, count(*) as count from error_instances where project_id = ? and date >= now() - interval 1 week group by lastactivity order by count(*) desc limit 1';
+    connection.query(queryString, [key], function(err, rows, fields) {
+        if (err) throw err;
 
-    res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
 
-    var result = new Object();
-    result = rows[0];
+        var result = new Object();
+        result = rows[0];
 
-    res.send(result);
-  });
+        res.send(result);
+    });
 });
 
 router.get(routeIdentifier + '/error_instances/weekly/:id', function(req, res, next) {
-  var key = req.params.id;
-  var queryString = 'SELECT * from error_instances where error_id = ? AND date >= now() - interval 1 week';
+    var key = req.params.id;
+    var queryString = 'SELECT * from error_instances where error_id = ? AND date >= now() - interval 1 week';
 
-  connection.query(queryString, [key], function(err, rows, fields) {
-    res.header("Access-Control-Allow-Origin", "*");
-    if (err) throw err;
-    res.json(rows);
+    connection.query(queryString, [key], function(err, rows, fields) {
+        res.header("Access-Control-Allow-Origin", "*");
+        if (err) throw err;
+        res.json(rows);
 
-  });
+    });
 });
 
 router.get(routeIdentifier + '/appruncount/weekly/:id', function(req, res, next) {
-  var data = {
-    'tag': 'appruncount_weekly',
-    'data': req.params.id
-  };
-  async.series([
-    function(cb) {
-      var ret = mq.publish(queueName, data);
-      cb();
-    }
-  ], function(err) {
-    var result = {
-      'state': 'success'
+    var data = {
+        'tag': 'appruncount_weekly',
+        'data': req.params.id
     };
-    res.send(result);
-  });
+    async.series([
+        function(cb) {
+            var ret = mq.publish(queueName, data);
+            cb();
+        }
+    ], function(err) {
+        var result = {
+            'state': 'success'
+        };
+        res.send(result);
+    });
 
 });
 
@@ -200,101 +200,97 @@ router.get(routeIdentifier + '/appruncount/weekly/:id', function(req, res, next)
 // Android v1 (Default UrQA)
 // Exception
 router.post(routeIdentifier + '/urqa/client/send/exception', function(req, res) {
-  res.status(200).send({
-    response: 200
-  });
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // Native Exception
 router.post(routeIdentifier + '/urqa/client/send/exception/native', function(req, res) {
-  res.status(200).send({
-    response: 200
-  });
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // Session
 router.post(routeIdentifier + '/urqa/client/connect', jsonParser, function(req, res) {
-  res.status(200).send({
-    response: 200
-  });
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // Key
 router.post(routeIdentifier + '/urqa/client/get_key', function(req, res) {
-  res.status(200).send({
-    response: 200
-  });
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // Android v2 (HoneyQA)
 // Exception
 router.post(routeIdentifier + '/api/v2/client/exception', jsonParser, function(req, res) {
-  if (dataChecker.androidExceptionCheck(req.body)) {
-    mq.publish('androidV2', req.body);
-    res.status(200).send({
-      response: 200
-    });
-  } else {
-    res.status(400).send({
-      response: 400
-    });
-  }
+    if (dataChecker.androidExceptionCheck(req.body)) {
+        mq.publish('androidV2', req.body);
+        res.status(200).send({
+            response: 200
+        });
+    } else {
+        res.status(400).send({
+            response: 400
+        });
+    }
 });
 
 // Native Exception
 router.post(routeIdentifier + '/api/v2/client/exception/native', function(req, res) {
-  res.status(200).send({
-    response: 200
-  });
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // Session
 router.post(routeIdentifier + '/api/v2/client/session', jsonParser, function(req, res) {
-  redisHandler.clientSession(res.body.apikey, res.body, function(err, res) {});
-  res.status(200).send({
-    response: 200
-  });
+    redisHandler.clientSession(res.body.apikey, res.body, function(err, res) {});
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // Key
 router.post(routeIdentifier + '/api/v2/client/key', function(req, res) {
-  res.status(200).send({
-    response: 200
-  });
+    res.status(200).send({
+        response: 200
+    });
 });
 
 // iOS
 // Exception
 router.post(routeIdentifier + '/api/ios/client/exception', jsonParser, function(req, res) {
-  if (dataChecker.iOSExceptionCheck(req.body)) {
-   
-    async.series([
-   
-    function(cb) {
-    mq.publish('iOSV1', req.body);
-    res.status(200).send({
-      response: 200
-    });
-    cb();
+    if (dataChecker.iOSExceptionCheck(req.body)) {
+        async.series([
+            function(cb) {
+                mq.publish('iOSV1', req.body);
+                res.status(200).send({
+                    response: 200
+                });
+                cb();
+            }
+        ]);
+    } else {
+        res.status(400).send({
+            response: 400
+        });
     }
-  
-  ]);
-    
-  } else {
-    res.status(400).send({
-      response: 400
-    });
-  }
 });
 
 // Session
 router.post(routeIdentifier + '/api/ios/client/session', jsonParser, function(req, res) {
-  // TODO : insert data to redis
-  // apikey / appversion / ios_version / model / carrier_name / country_code
-  redisHandler.clientSession(res.body.apikey, res.body, function(err, res) {});
-  res.status(200).send({
-    response: 200
-  });
+    // TODO : insert data to redis
+    // apikey / appversion / ios_version / model / carrier_name / country_code
+    redisHandler.clientSession(res.body.apikey, res.body, function(err, res) {});
+    res.status(200).send({
+        response: 200
+    });
 });
 
 module.exports = router;
--- handler/mq_pubhandler.js
+++ handler/mq_pubhandler.js
@@ -1,36 +1,39 @@
 //'use strict';
-var gk   = require('../common');
+var gk = require('../common');
 var amqp = require('amqp');
 
-exports.LOGER_EXCHANGE_NAME     = gk.config.mqExchangeName;
-exports.LOGER_QUEUE_NAME        = gk.config.mqQueueName;
-var connection                  = amqp.createConnection(gk.config.mq);
+exports.LOGER_EXCHANGE_NAME = gk.config.mqExchangeName;
+exports.LOGER_QUEUE_NAME = gk.config.mqQueueName;
+var connection = amqp.createConnection(gk.config.mq);
 
-var self            = this;
-var _queue          = [];
-var _isReady        = false;
+var self = this;
+var _queue = [];
+var _isReady = false;
 var MAX_RETRY_COUNT = 5;
-var retry           = 3;
+var retry = 3;
 
 
-connection.addListener('ready', function () {
+connection.addListener('ready', function() {
   console.log("connected to " + connection.serverProperties.product);
-  var exchange = connection.exchange(exports.LOGER_EXCHANGE_NAME ,{ type:"fanout", durable:true } );
+  var exchange = connection.exchange(exports.LOGER_EXCHANGE_NAME, {
+    type: "fanout",
+    durable: true
+  });
   _queue[exports.LOGER_QUEUE_NAME] = exchange;
   _isReady = true;
 });
 
 
-process.addListener('exit', function () {
-  console.log('Queue Exit '); 
+process.addListener('exit', function() {
+  console.log('Queue Exit ');
 });
 
 
 exports.publish = function(queueName, msg) {
   var retVal;
   retry = retry || 0;
-  if (retry > MAX_RETRY_COUNT){
-    return; 
+  if (retry > MAX_RETRY_COUNT) {
+    return;
   }
 
   if (_queue.hasOwnProperty(queueName)) {
@@ -41,8 +44,6 @@ exports.publish = function(queueName, msg) {
     // TODO: binding queue automatically
     _isReady = false;
   }
-  
+
   //global.gc(true);
 };
-
-
